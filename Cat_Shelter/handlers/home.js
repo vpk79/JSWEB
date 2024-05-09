5a8dc041cb4cdc34b31e34eeecb5429689aadec0
vpk79 <someone_bul@yahoo.com>
2024-05-09 22:51:36 +0300

    up

--- Cat_Shelter/handlers/home.js
+++ Cat_Shelter/handlers/home.js
@@ -8,7 +8,29 @@ module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
 
     if(pathname === '/' && req.method === 'GET'){
+        let filePath = path.normalize(
+            path.join(__dirname, '../views/home/index.html')
+        );
 
+        fs.readFile(filePath,(err, data) => {
+            if(err){
+                console.log(err);
+                res.writeHead(400, {
+                    'Content-Type': 'text/plain'
+                });
+
+                res.write('404 Not found');
+                res.end();
+                return;
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+
+            res.write(data);
+            res.end();
+        })
     } else {
         return true;
     }
--- Cat_Shelter/index.js
+++ Cat_Shelter/index.js
@@ -1,13 +1,14 @@
 const http = require('http');
 const port = 3000;
+const handlers = require('./handlers');
 
 http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-
-    res.write('Hello JS World!');
-    res.end();
+    
+    for(let handler of handlers){
+        if(!handler(req,res)) {
+            break;
+        }
+    }
 }).listen(port);
 
 console.log('Server is working at port 3000...');
