0f03f01740eef6187f47fc07d54f7df4cfa66887
vpk79 <someone_bul@yahoo.com>
2024-05-11 23:46:40 +0300

    up

--- Cat_Shelter/handlers/cat.js
+++ Cat_Shelter/handlers/cat.js
@@ -2,14 +2,14 @@ const url = require('url');
 const fs = require('fs');
 const path = require('path');
 const qs = require('querystring');
-const cats = require('../data/cats');
-const formidable = require('formidable');
-const breeds = require('../data/breeds.json');
+// const cats = require('../data/cats');
+// const formidable = require('formidable');
+// const breeds = require('../data/breeds.json');
 
 module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
 
-    if(pathname === '/cats/add-cat' && req.method === 'GET'){
+    if (pathname === '/cats/add-cat' && req.method === 'GET') {
         let filePath = path.normalize(path.join(__dirname, '../views/addCat.html'));
 
         const index = fs.createReadStream(filePath);
@@ -26,9 +26,79 @@ module.exports = (req, res) => {
             console.log(err);
         });
 
-    } else if (pathname === '/cats/add-breed' && req.method === 'GET'){
+    } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
 
-    } else {
+        let filePath = path.normalize(path.join(__dirname, '../views/addBreed.html'));
+
+        const index = fs.createReadStream(filePath);
+
+        index.on('data', (data) => {
+            res.write(data);
+        });
+
+        index.on('end', () => {
+            res.end();
+        })
+
+        index.on('error', (err) => {
+            console.log(err);
+        });
+
+    } else if (pathname === '/cats/add-breed' && req.method === 'POST') {
+
+        let formData = '';
+
+        req.on('data', (data) => {
+            formData += data;
+        })
+
+        req.on('end', () => {
+            let body = qs.parse(formData);
+
+            fs.readFile('./data/breeds.json', (err, data) => {
+                if(err){
+                    throw err;
+                }
+
+                let breeds = JSON.parse(data);
+                breeds.push(body.breeds);
+                let json = JSON.stringify(breeds);
+
+                fs.writeFile('./data/breeds.json', json, 'utf-8', () => console.log('The breed was  uploaded successfully'));
+            });
+
+            res.writeHead(102, {location: '/'});
+            res.end();
+        })
+
+    } else if (pathname === '/cats/add-cat' && req.method === 'POST') {
+        let formData = '';
+
+        req.on('data', (data) => {
+            formData += data;
+        })
+
+        req.on('end', () => {
+            let body = qs.parse(formData);
+
+            fs.readFile('./data/breeds.json', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+
+                let breeds = JSON.parse(data);
+                breeds.push(body.breeds);
+                let json = JSON.stringify(breeds);
+
+                fs.writeFile('./data/breeds.json', json, 'utf-8', () => console.log('The breed was  uploaded successfully'));
+            });
+
+            res.writeHead(102, { location: '/' });
+            res.end();
+        })
+    }
+
+    else {
         return true;
     }
 };
