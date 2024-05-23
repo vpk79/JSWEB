5fd50bdb0b258a739da17618b45f53fdf8e9b9fe
vpk79 <someone_bul@yahoo.com>
2024-05-24 00:05:25 +0300

    up

--- Movie_magic/src/index.js
+++ Movie_magic/src/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
+// set handlebars as view engine and configurate it
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }))
@@ -13,9 +14,15 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'))
 
+// set express to user folder public for all static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// handle entry paths
 app.get('/', (req, res) => {
     res.render('home', {layout: false})
 });
 
+
+// run server on port
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
 
