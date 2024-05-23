6651c73cb8a359581a077a32ccdbaee076bd4cc7
vpk79 <someone_bul@yahoo.com>
2024-05-23 12:02:31 +0300

    Update index.js

--- new_project/index.js
+++ new_project/index.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.header({
+        'Content-Type': 'text/plain'
+    })
+    res.send('<h1>Hello World</h1>');
+    
 })
 
+app.listen(5000);
+console.log('Server is listening on port 5000');
