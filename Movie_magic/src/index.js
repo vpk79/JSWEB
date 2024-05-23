d00fad9f183144626683af0a55eb7faef29575e0
vpk79 <someone_bul@yahoo.com>
2024-05-23 23:33:55 +0300

    Update index.js

--- Movie_magic/src/index.js
+++ Movie_magic/src/index.js
@@ -1,8 +1,15 @@
 
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 const port = 5000;
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}))
+
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     res.send('Home page')
