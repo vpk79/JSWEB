5a93abecdf945cc21f8ab98316598de6232d76b2
vpk79 <someone_bul@yahoo.com>
2024-05-24 22:07:50 +0300

    up

--- Movie_magic/src/controllers/homeController.js
+++ Movie_magic/src/controllers/homeController.js
@@ -5,15 +5,22 @@ const movieService = require('../services/movieService');
 router.get('/', (req, res) => {
     const movies = movieService.getAll();
 
-    res.render('home', {movies})
+    res.render('home', { movies })
 });
 
 router.get('/about', (req, res) => {
     res.render('about');
 });
 
+router.get('/search', (req, res) => {
+    const movies = movieService.getAll();
+    res.render('search', { movies });
+})
+
 router.get('/404', (req, res) => {
     res.render('404');
 });
+
+
 
 module.exports = router;
