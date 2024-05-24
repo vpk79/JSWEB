52f4839fbf393319fdfc5e6a82533dc9820cf388
vpk79 <someone_bul@yahoo.com>
2024-05-24 18:49:44 +0300

    up

--- Movie_magic/src/config/configExpress.js
+++ Movie_magic/src/config/configExpress.js
@@ -5,6 +5,9 @@ function configExpress(app) {
     // set express to user folder public for all static files
     app.use(express.static(path.resolve('src/public')));
 
+    // intercept and parse any data coming with the request
+    app.use(express.urlencoded({ extended: false }))
+
     return app;
 }
 
--- Movie_magic/src/controllers/movieController.js
+++ Movie_magic/src/controllers/movieController.js
@@ -3,7 +3,16 @@ const router = require('express').Router();
 
 router.get('/create', (req, res) => {
     res.render('create');
-})
+});
+
+router.post('/create', (req, res) => {
+   const newMovie = req.body;
+   
+    res.send('Created new movie')
+});
+
+
+
 
 
 module.exports = router;
