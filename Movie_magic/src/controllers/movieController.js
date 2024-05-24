b48e4f71ed0f7de9292f9efde23e6cb1ed1a9f4f
vpk79 <someone_bul@yahoo.com>
2024-05-24 20:22:53 +0300

    up

--- Movie_magic/src/controllers/movieController.js
+++ Movie_magic/src/controllers/movieController.js
@@ -8,7 +8,7 @@ router.get('/create', (req, res) => {
 router.post('/create', (req, res) => {
    const newMovie = req.body;
     movieService.create(newMovie);
-    res.send('Created new movie')
+    res.redirect('/');
 });
 
 
