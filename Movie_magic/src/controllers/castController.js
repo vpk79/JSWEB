5ef1a8e6260cb91effa6abce1630f7f8d26385ac
vpk79 <someone_bul@yahoo.com>
2024-05-25 15:18:14 +0300

    up

--- Movie_magic/src/controllers/castController.js
+++ Movie_magic/src/controllers/castController.js
@@ -4,4 +4,11 @@ router.get('/create', (req, res) => {
     res.render('cast/create');
 })
 
+router.post('/create', (req, res) => {
+    const body = req.body;
+    console.log(body);
+
+    res.redirect('/');
+})
+
 module.exports = router;
