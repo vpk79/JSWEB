ec7bad592fed6e8c1ec9345ce84bac3c99b47611
vpk79 <someone_bul@yahoo.com>
2024-05-25 17:56:26 +0300

    up

--- Movie_magic/src/controllers/movieController.js
+++ Movie_magic/src/controllers/movieController.js
@@ -25,6 +25,10 @@ router.get('/movies/:movieId', async (req, res) => {
     res.render('details', { movie });
 })
 
+router.get('/movies/:movieId/attach', (req, res) => {
+    res.render('movie/attach');
+});
+
 
 
 module.exports = router;
