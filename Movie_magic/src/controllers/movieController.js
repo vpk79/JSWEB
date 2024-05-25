37585de7f74f8b909580eff53c69515d1ef7e30e
vpk79 <someone_bul@yahoo.com>
2024-05-25 18:24:06 +0300

    up

--- Movie_magic/src/controllers/movieController.js
+++ Movie_magic/src/controllers/movieController.js
@@ -21,12 +21,13 @@ router.get('/movies/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId).lean();
     movie.movieStars = '&#x2605;'.repeat(movie.rating);
-    
+
     res.render('details', { movie });
 })
 
-router.get('/movies/:movieId/attach', (req, res) => {
-    res.render('movie/attach');
+router.get('/movies/:movieId/attach', async (req, res) => {
+    const movie = await movieService.getOne(req.params.movieId).lean();
+    res.render('movie/attach', { ...movie });
 });
 
 
