c49cf21e4c33d65d01e42a028443df7dd32e967a
vpk79 <someone_bul@yahoo.com>
2024-05-25 11:55:51 +0300

    up

--- Movie_magic/src/index.js
+++ Movie_magic/src/index.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const configHandlebars = require('./config/configHandlebars');
 const configExpress = require('./config/configExpress');
@@ -14,8 +15,13 @@ configExpress(app);
 
 app.use(routes);
 
+mongoose.connect(`mongodb://localhost:27017/magic-movies`)
+    .then(() => {
+        console.log(`DB Connected`);
+        // run server on port
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    })
+    .catch(err => console.log('Cannot connect to DB'));
 
 
-// run server on port
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
 
