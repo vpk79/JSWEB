361e12916d8a3d74fc8b8561cade23b1c906eaa8
vpk79 <someone_bul@yahoo.com>
2024-05-25 23:12:51 +0300

    Update movieService.js

--- Movie_magic/src/services/movieService.js
+++ Movie_magic/src/services/movieService.js
@@ -6,26 +6,11 @@ exports.getAll = () => {
 }
 
 exports.search =  (title, genre, year) => {
-    // let result = await Movie.find().lean();
-
     let query = {};
 
     if(title){ query.title = new RegExp(title, 'i')}
     if(genre) {query.genre = genre.toLowerCase()}
     if(year){ query.year = year}
-    // if (title) {
-    //     result = result.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
-    // }
-
-    // if (genre) {
-    //     result = result.filter(movie => movie.genre.toLowerCase() === genre.toLowerCase());
-    // }
-
-    // if (year) {
-    //     result = result.filter(movie => movie.year === year);
-    // }
-
-    // return result;
 
     return Movie.find(query);
 }
