ae53f9bd32a2778338920823ac63c915e1af3af5
vpk79 <someone_bul@yahoo.com>
2024-05-26 22:08:06 +0300

    up

--- Movie_magic/src/services/authService.js
+++ Movie_magic/src/services/authService.js
@@ -1,7 +1,20 @@
-const User = require('../Models/User')
+const User = require('../Models/User');
+const bcrypt = require('bcrypt');
 
 exports.register = (userData) => User.create(userData);
 
-exports.login = (email, password) => {
-    
+exports.login = async (email, password) => {
+    // get user from db
+    const user = await  User.findOne({email});
+
+    // check if user exists
+    if(!user) {
+        throw new Error('User doesn\'t exist')
+    }
+
+    // check if password is valid
+    const isValid = await bcrypt.compare(password, user.password);
+    if(!isValid){
+        throw new Error('Password doesn\'t match');
+    }
 }
