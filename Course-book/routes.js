228ebf4b939f7d08ced35df3617d99dbbcc8206b
vpk79 <someone_bul@yahoo.com>
2024-05-30 19:57:32 +0300

    up

--- Course-book/routes.js
+++ Course-book/routes.js
@@ -1,7 +1,11 @@
 // с помощта на Router() създаваме модулярен рутер
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
+const authController = require('./controllers/authController');
 
 router.use(homeController);
+router.use('/auth', authController);
+
+
 
 module.exports = router;
