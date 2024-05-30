e3f3521c643c7824740c297896a170bddc9e6c2e
vpk79 <someone_bul@yahoo.com>
2024-05-31 00:01:15 +0300

    up

--- Course-book/services/authService.js
+++ Course-book/services/authService.js
@@ -10,12 +10,12 @@ exports.login = async ({email, password}) => {
     const user = await User.findOne({email});  // намираме юзера по email
 
     if(!user){                                                          
-        throw new Error('Invalid username or password')  
+        throw new Error('Invalid email or password')  
     }
 
     const isValid = await bcrypt.compare(password, user.password);  // проверяваме дали паролата му е вярна
 
     if(!isValid){
-        throw new Error('Invalid username or password')
+        throw new Error('Invalid email or password')
     }
 }
