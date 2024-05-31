ece547a5ed72e509d5782ca9af5ff43b313de72d
vpk79 <someone_bul@yahoo.com>
2024-05-31 11:52:46 +0300

    up

--- Course-book/services/authService.js
+++ Course-book/services/authService.js
@@ -3,25 +3,31 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 
-exports.register = (userData) => {
-    if(userData.password !== userData.rePassword){
+exports.register = async (userData) => {
+    if (userData.password !== userData.rePassword) {
         throw new Error('Password mismatch');
+    }
+
+    const user = await user.find({ email: userData.email });
+
+    if(user){
+        throw new Error('User already exist');
     }
 
     return User.create(userData); // връща promise със обработените данни
 }
 
-exports.login = async ({email, password}) => {
+exports.login = async ({ email, password }) => {
 
-    const user = await User.findOne({email});  // намираме юзера по email
+    const user = await User.findOne({ email });  // намираме юзера по email
 
-    if(!user){                                                          
-        throw new Error('Invalid email or password')  
+    if (!user) {
+        throw new Error('Invalid email or password')
     }
 
     const isValid = await bcrypt.compare(password, user.password);  // проверяваме дали паролата му е вярна
 
-    if(!isValid){
+    if (!isValid) {
         throw new Error('Invalid email or password')
     }
 }
